92c14e30185a180e95fa1977380e8329d600d536
cthomson <cthomson@student.saintaug.nsw.edu.au>
2024-05-15 11:48:35 +1000

    changes

--- password_validator.py
+++ password_validator.py
@@ -1,15 +1,9 @@
 #password_validator
 import gooeypie as gp
 
-def password_check(password):
-    # Define the criteria for password strength
-    length_criteria = 8
-    uppercase = False
-    digits = False
-    has_special_char = False
-    special_chars = "!@#$%^&*()_+[]{}?:;|'\"\\,./~`"
-
-    # Check each character in the password
+#special_chars = "!@#$%^&*()_+[]{}?:;|'\"\\,./~`"
+# Check each character in the password
+def strenth_test(password):
     security = 0
     for char in password:
         if len >= 8:
@@ -19,16 +13,13 @@ def password_check(password):
         elif char.isdigit():
             digits = True
             security += 1
-        elif char in special_chars:
-            has_special_char = True
-            security += 1
     
     # Check overall strength
     strength = "Weak"
-    if len(password) >= length_criteria and uppercase and lowercase and digits and has_special_char:
-        strength = "Strong"
-    elif len(password) >= length_criteria and (uppercase or lowercase or digits):
+    if security <= 3:
         strength = "Medium"
+    elif len(password) <= 5:
+        strength = "Good Stuff"
 
     # Display strength
     strength_lbl.set_text(f"Strength: {strength}")
@@ -39,12 +30,41 @@ pass_lbl = gp.Label(app, "Password")
 pass_inp = gp.Secret(app)
 strength_lbl = gp.Label(app, "Strength: ")
 
-check_btn = gp.Button(app, "Check Strength", password_check, pass_inp)
+check_btn = gp.Button(app, "Check Strength", pass_inp)
 
 app.set_grid(4, 2)
 app.add(pass_lbl, 1, 1)
 app.add(pass_inp, 1, 2)
 app.add(check_btn, 1, 3)
 app.add(strength_lbl, 2, 2)
+
+app.run()
+#################
+
+def on_text_change(event):
+    text = text_box.text
+    print(text)
+
+    # run the logic for checks
+    # ren check for length
+    if len(text) < 10:
+        print("Text under 10")
+
+    # check for symbols
+
+
+    # check against list of common passwords
+    
+
+app = gp.GooeyPieApp('Might be useful for your assessment')
+
+text_box = gp.Textbox(app)
+text_box.add_event_listener('change', on_text_change)
+
+label = gp.Label(app, 'blank')
+
+app.set_grid(2, 1)
+app.add(text_box, 1, 1)
+app.add(label, 2, 1)
 
 app.run()
