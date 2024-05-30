81009f209b2488f81d6b62e5492c716844db339d
cthomson <cthomson@student.saintaug.nsw.edu.au>
2024-05-30 11:45:27 +1000

    commit

--- password_validator.py
+++ password_validator.py
@@ -9,7 +9,6 @@ def special_chars(s):
         return True
     return False
 
-
 # Check each character in the password
 def password_checker(event):
     password = pass_inp.text
@@ -31,50 +30,96 @@ def password_checker(event):
 def open_second_window(event):
     second_win.show()
 
-#################
-
 def say_hello(event):
     hello_lbl.text = 'Check your passwords security'
-
-
-# Check different attributes to see if password is good (plan for future use class attributes)    
-
-    # check for symbols
-
-    # check against list of common passwords
-    
-#text_box.background_colour = 'black'
 
 app = gp.GooeyPieApp('Password Validator')
 app.set_size(300, 200)
 
-hello_btn = gp.Button(app, 'Open Validator', say_hello)
-hello_lbl = gp.Label(app, '')
+hello_lbl = gp.Label(app, 'Welcome to the Password Validator App!')
+open_button = gp.Button(app, 'Open Validator', open_second_window)
 
 app.set_grid(6, 2)
-app.add(hello_btn, 1, 1, column_span=2, align='center')
-app.add(hello_lbl, 2, 1, column_span=2, align='center')
-
-# Main window and password check UI
-pass_lbl = gp.Label(app, "Password")
-pass_inp = gp.Secret(app)
-login_btn = gp.Button(app, 'Check Password', password_checker)
-status_lbl = gp.Label(app, '')
-
-app.add(pass_lbl, 3, 1)
-app.add(pass_inp, 3, 2)
-app.add(login_btn, 4, 2)
-app.add(status_lbl, 5, 1, column_span=2)
-
-# Open second window button in the main app
-open_button = gp.Button(app, 'Open Second Window', open_second_window)
-app.add(open_button, 6, 1, column_span=2, align='center')
+app.add(hello_lbl, 1, 1, column_span=2, align='center')
+app.add(open_button, 2, 1, column_span=2, align='center')
 
 # Second window setup
-second_win = gp.Window(app, 'Second Window')
+second_win = gp.Window(app, 'Password Validator')
 second_win.set_size(300, 200)
-label = gp.Label(second_win, 'This is the second window')
-second_win.set_grid(1, 1)
-second_win.add(label, 1, 1)
+
+pass_lbl = gp.Label(second_win, "Password")
+pass_inp = gp.Secret(second_win)
+login_btn = gp.Button(second_win, 'Check Password', password_checker)
+status_lbl = gp.Label(second_win, '')
+
+second_win.set_grid(4, 2)
+second_win.add(pass_lbl, 1, 1)
+second_win.add(pass_inp, 1, 2)
+second_win.add(login_btn, 2, 2)
+second_win.add(status_lbl, 3, 1, column_span=2)
 
 app.run()
+
+###################################################################
+
+
+
+
+
+# def special_chars(s):
+#     pattern = re.compile(r'[!@#$%^&*(),.?":{}|<>]')
+#     # Search for the pattern in the string
+#     if pattern.search(s):
+#         return True
+#     return False
+
+# # Check each character in the password
+# def password_checker(event):
+#     password = pass_inp.text
+#     if len(password) <= 0: #password length
+#         status_lbl.text = 'Please enter a password!'
+#     elif len(password) >= 0 and len(password) <= 4:
+#         status_lbl.text = 'Please make the password at least 5 characters long'
+#     elif password.isdigit() == True: # just digits
+#         status_lbl.text = 'Try having more letters'
+#     elif password.isalpha() == True: # just letters
+#         status_lbl.text = 'Try having more numbers'
+#     elif not special_chars(password): # no special characters
+#         status_lbl.text = 'Try including a special character'
+#     else: # Requirements all met
+#         status_lbl.text = 'PERFECT'
+
+#     status_lbl.update()
+    
+# def open_second_window(event):
+#     second_win.show()
+
+# def say_hello(event):
+#     hello_lbl.text = 'Check your passwords security'
+
+# app = gp.GooeyPieApp('Password Validator')
+# app.set_size(300, 200)
+
+# hello_lbl = gp.Label(app, 'Welcome to the Password Validator App!')
+# open_button = gp.Button(app, 'Open Validator', open_second_window)
+
+# app.set_grid(6, 2)
+# app.add(hello_lbl, 1, 1, column_span=2, align='center')
+# app.add(open_button, 2, 1, column_span=2, align='center')
+
+# # Second window setup
+# second_win = gp.Window(app, 'Password Validator')
+# second_win.set_size(300, 200)
+
+# pass_lbl = gp.Label(second_win, "Password")
+# pass_inp = gp.Secret(second_win)
+# login_btn = gp.Button(second_win, 'Check Password', password_checker)
+# status_lbl = gp.Label(second_win, '')
+
+# second_win.set_grid(4, 2)
+# second_win.add(pass_lbl, 1, 1)
+# second_win.add(pass_inp, 1, 2)
+# second_win.add(login_btn, 2, 2)
+# second_win.add(status_lbl, 3, 1, column_span=2)
+
+# app.run()
