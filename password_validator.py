f30a8cc9abcc67fcb7fa0f72b61cb810a5dd9982
cthomson <cthomson@student.saintaug.nsw.edu.au>
2024-06-03 09:20:42 +1000

    commit

--- password_validator.py
+++ password_validator.py
@@ -30,18 +30,30 @@ def password_checker(event):
 def open_second_window(event):
     second_win.show()
 
+def open_faq_window(event):
+    faq_win.show()
+
+def open_about_window(event):
+    about_win.show()
+
 def say_hello(event):
-    hello_lbl.text = 'Check your passwords security'
+    hello_ttl.text = 'Check your passwords security'
 
 app = gp.GooeyPieApp('Password Validator')
 app.set_size(300, 200)
 
-hello_lbl = gp.Label(app, 'Welcome to the Password Validator App!')
+hello_ttl = gp.Label(app, 'Welcome to SalusPekt!')
+
 open_button = gp.Button(app, 'Open Validator', open_second_window)
+about_button = gp.Button(app, 'About', open_about_window)
+faq_button = gp.Button(app, '?', open_faq_window)
 
 app.set_grid(6, 2)
-app.add(hello_lbl, 1, 1, column_span=2, align='center')
-app.add(open_button, 2, 1, column_span=2, align='center')
+app.add(hello_ttl, 2, 1, column_span=2, align='center')
+app.add(open_button, 6, 1, column_span=2, align='left')
+
+app.add(about_button, 5, 1, column_span=2, align='right')
+app.add(faq_button, 4, 1, column_span=2, align='right')
 
 # Second window setup
 second_win = gp.Window(app, 'Password Validator')
@@ -58,13 +70,26 @@ second_win.add(pass_inp, 1, 2)
 second_win.add(login_btn, 2, 2)
 second_win.add(status_lbl, 3, 1, column_span=2)
 
-app.run()
+# Third window (faq) setup
+
+faq_win = gp.Window(app, '?')
+faq_win.set_size(300, 200)
+
+faq_win.set_grid(6, 2)
+
+# Fourth Window (About) setup
+
+about_win = gp.Window(app, 'About')
+about_win.set_size(300, 200)
+
+about_win.set_grid(6, 2)
+
 
 ###################################################################
 
+app.run()
 
-
-
+###################################################################
 
 # def special_chars(s):
 #     pattern = re.compile(r'[!@#$%^&*(),.?":{}|<>]')
